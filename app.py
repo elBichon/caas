3c69ea1883c6c7b54dd53c33065d6905ca3a6bb0
elBichon <reuglewiczjeanedouard@gmail.com>
2021-10-05 08:47:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from website import create_app
+import os, ast
+import pandas as pd
 
-if __name__ == "__main__":
-    print('hello world')
-    app = create_app()
-    app.run(debug=True)
+
+target = "Purchase"
+usecase = "classification"
+
+dataset_path = "https://raw.githubusercontent.com/elBichon/easyML/master/juice.csv"
+data = pd.read_csv(dataset_path)
+data.head()
+
+if usecase == 'regression':
+    from pycaret.regression import *
+elif usecase == 'classification':
+    from pycaret.classification import *
+
+exp1 = setup(data, target = target, session_id=123, silent=True, html=False, log_experiment=True, experiment_name='exp_github')
+
+best = compare_models()
+
+best_model = finalize_model(best)
+
+save_model(best_model, 'model')
+
+logs_exp_github = get_logs(save=True)
